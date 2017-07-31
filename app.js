f86f6d75648251103f680ff4bb366b83db445344
Jorge Reyero <jorgereyero@gmail.com>
2017-07-31 13:08:42 +0200

    add geo location package

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ var requestIp = require('request-ip');
 app.use(requestIp.mw())
 
 app.get('/', function (req, res) {
-  console.log(req.clientIp);
-  res.send(req.clientIp);
+  var ip = req.clientIp;
+  var geo = geoip.lookup(ip);
+  res.send(geo);
 });
 
 app.listen(process.env.PORT || 3000, function () {
