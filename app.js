d32f631dcc94e5d60a68cb19ad50cb06cda5141c
Jorge Reyero <jorgereyero@gmail.com>
2017-07-31 12:26:08 +0200

    fix port error

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.get('/', function (req, res) {
   res.send(req.clientIp);
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
 });
